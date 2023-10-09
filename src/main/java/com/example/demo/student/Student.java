5e74809b1ba55ad9605b4527df5d39de92bec852
gcastillo <gcastillo.ent@gmail.com>
2023-10-09 15:49:50 -0500

    upload code

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,103 @@
+package com.example.demo.student;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+//Contains Student information with getters, setters, and toString()
+
+//@Entity is used with @Transactional and postgreSQL to manage updates to DB
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy= GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private String email;
+
+    //@Transient annotates that 'age' doesn't need to be in the DBMS and can be calculated using 'dob'
+    @Transient
+    private Integer age;
+
+
+
+    public Student() {
+
+    }
+
+    public Student(Long id, String name, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, LocalDate dob, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
